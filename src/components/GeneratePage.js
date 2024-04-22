ed47897022de9e54610390b4cca8d82f46e0136f
Suraj Goud Rachamalla <surajgoud36@gmail.com>
2024-04-22 03:05:35 -0400

    Output format needs to be fixed

--- src/components/GeneratePage.js
+++ src/components/GeneratePage.js
@@ -1,9 +1,54 @@
-
-import "../styling/GeneratePage.css"
-import logo from  '../assets/pen.jpg'
+import "../styling/GeneratePage.css";
+import logo from "../assets/pen.jpg";
 import React, { useState, useEffect } from "react";
 
+let paths = [];
+let path = [];
+let edges = new Map();
+let aset = new Set();
+let finalPaths = new Map();
+let counter = 1;
+let fpath = [];
+let fpath1 = [];
+let fpath2 = []; // final test paths for edge coverage
+let fpath3 = []; // final test paths for node coverage
+let temp = []; // Test path edge pairs
 
+class Graph {
+  constructor() {
+    this.adjList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjList[vertex]) {
+      this.adjList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjList[vertex1]) {
+      this.addVertex(vertex1);
+    }
+    if (!this.adjList[vertex2]) {
+      this.addVertex(vertex2);
+    }
+    this.adjList[vertex1].push(vertex2);
+  }
+
+  display() {
+    console.log(this.adjList);
+  }
+}
+
+function createGraph(graph, vertex, edges) {
+  console.log(`Vertex: ${vertex}, Edges: ${edges}`);
+  let neighbors = edges.split(",");
+  console.log(neighbors);
+  graph.addVertex(vertex);
+  for (let i = 0; i < neighbors.length; i++) {
+    graph.addEdge(vertex, neighbors[i]);
+  }
+}
 
 function GeneratePage() {
   // Initialize state with 6 rows
@@ -18,12 +63,89 @@ function GeneratePage() {
 
   // State for selected option
   const [selectedOption, setSelectedOption] = useState(null);
-
+  const [tR, setTR] = useState("");
+  const [tP, setTP] = useState("");
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
   const handleClick = () => {
     // Handle button click
+    let startNode = "";
+    let endNode = "";
+    console.log("Current state of rows:", rows);
+    const graph = new Graph();
+    // If you need to perform additional logic, like validating data or preparing data for an API call, you can do that here.
+    rows.forEach((row, index) => {
+      // console.log(`Row ${index + 1} - Column 1: ${row.column1}, Column 2: ${row.column2}`);
+      //console.log(`Type of Column 1: ${typeof row.column1}, Type of Column 2: ${typeof row.column2}`);
+      // console.log(index);
+      if (index === 0) {
+        startNode = row.column1;
+      }
+      endNode = row.column1;
+      createGraph(graph, row.column1, row.column2);
+    });
+    console.log(startNode);
+    console.log(endNode);
+    graph.display();
+    console.log(selectedOption);
+    findTestPaths(graph, startNode, endNode);
+    generateFinalPaths();
+    pathEdgePairCoverage();
+
+    if (selectedOption === "edgePairCoverage") {
+      let tempRes = [];
+      for (let i = 0; i < fpath.length; i++) {
+        if (temp.includes(i)) {
+        } else {
+          tempRes.push(fpath[i]);
+        }
+      }
+      let str = tempRes.join(" ");
+      setTP(str);
+      let edgePPairs = edgePairs(graph);
+      let str1 = edgePPairs.join(" ");
+      setTR(str1);
+    } else if (selectedOption === "edgeCoverage") {
+      let temp1 = pathEdgeCoverage(fpath2); // test paths for edge coverage, remove the indices from fpath2
+      let tempRes = [];
+      for (let i = 0; i < fpath2.length; i++) {
+        if (temp1.includes(i)) {
+        } else {
+          tempRes.push(fpath2[i]);
+        }
+      }
+      let str = tempRes.join(" ");
+      setTP(str);
+      let edges1 = edgesF(graph);
+      let str1 = edges1.join(" ");
+      setTR(str1);
+    } else {
+      let temp2 = pathNodeCoverage(fpath3);
+      let tempRes = [];
+      for (let i = 0; i < fpath3.length; i++) {
+        if (temp2.includes(i)) {
+        } else {
+          tempRes.push(fpath3[i]);
+        }
+      }
+      let str = tempRes.join(" ");
+      setTP(str);
+      let nodes1 = nodes(graph);
+      let str1 = nodes1.join(" ");
+      setTR(str1);
+    }
+     paths = [];
+     path = [];
+     edges = new Map();
+     aset = new Set();
+     finalPaths = new Map();
+     counter = 1;
+     fpath = [];
+     fpath1 = [];
+     fpath2 = []; // final test paths for edge coverage
+     fpath3 = []; // final test paths for node coverage
+     temp = []; // Test path edge pairs
   };
   // Function to handle adding a new row
   const handleAddRow = () => {
@@ -47,12 +169,18 @@ function GeneratePage() {
   // Ensure that there are always at least 6 rows on initial load
   useEffect(() => {
     if (rows.length < 6) {
-      setRows([
-        ...rows,
-        ...Array(6 - rows.length).fill({ column1: "", column2: "" }),
-      ]);
+      setRows((prevRows) => {
+        if (prevRows.length < 6) {
+          return [
+            ...prevRows,
+            ...Array(6 - prevRows.length).fill({ column1: "", column2: "" }),
+          ];
+        }
+        return prevRows;
+      });
     }
-  }, [rows]); // Include rows as dependency to trigger effect on rows change
+  }, []); // Empty dependency array to only run once on mount
+  // Include rows as dependency to trigger effect on rows change
 
   // Inline styles
   const tabStyle = {
@@ -207,31 +335,31 @@ function GeneratePage() {
           <input
             type="radio"
             name="option"
-            value="option1"
-            checked={selectedOption === "option1"}
+            value="nodeCoverage"
+            checked={selectedOption === "nodeCoverage"}
             onChange={handleOptionChange}
           />{" "}
-          Option 1
+          nodeCoverage
         </label>
         <label style={{ marginRight: "20px" }}>
           <input
             type="radio"
             name="option"
-            value="option2"
-            checked={selectedOption === "option2"}
+            value="edgeCoverage"
+            checked={selectedOption === "edgeCoverage"}
             onChange={handleOptionChange}
           />{" "}
-          Option 2
+          edgeCoverage
         </label>
         <label>
           <input
             type="radio"
             name="option"
-            value="option3"
-            checked={selectedOption === "option3"}
+            value="edgePairCoverage"
+            checked={selectedOption === "edgePairCoverage"}
             onChange={handleOptionChange}
           />{" "}
-          Option 3
+          edgePairCoverage
         </label>
 
         <button style={{ marginLeft: "20px" }} onClick={handleClick}>
@@ -241,18 +369,501 @@ function GeneratePage() {
       <div className="grey-background">
         <h4>{selectedOption}</h4>
         <div className="container">
-      <div className="text-area-container">
-        <h5 className="heading">Test Requirements</h5>
-        <textarea className="text-area" placeholder="Enter test requirements here..."></textarea>
-      </div>
-      <div className="text-area-container">
-        <h5 className="heading">TestPaths</h5>
-        <textarea className="text-area" placeholder="Enter text fields here..."></textarea>
-      </div>
-    </div>
+          <div className="text-area-container">
+            <h5 className="heading">Test Requirements</h5>
+            <textarea
+              className="text-area"
+              placeholder="Enter test requirements here..."
+              value={tR}
+              onChange={(e) => setTR(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="text-area-container">
+            <h5 className="heading">TestPaths</h5>
+            <textarea
+              className="text-area"
+              placeholder="Enter text fields here..."
+              value={tP}
+              onChange={(e) => setTP(e.target.value)}
+            ></textarea>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
+function findTestPaths(graph, startNode, endNode) {
+  dfs(graph, startNode, endNode, edges, path, paths);
+  console.log(path);
+  console.log(paths);
+  let arrayFromSet = [...aset];
+  console.log(arrayFromSet);
+  finalPaths.forEach((value, key) => {
+    console.log(key + " => " + value);
+  });
+  //console.log(edges);
+  //console.log(finalPaths.get(5)[1]);
+}
+function dfs(graph, currentNode, endNode, edges, paths, path) {
+  path.push(currentNode);
+  if (currentNode === endNode) {
+    paths.push(path);
+    aset.add(path);
+    finalPaths.set(counter, [...path]);
+    counter = counter + 1;
+    console.log(path);
+  } else {
+    let aLen = graph.adjList[currentNode].length;
+    //console.log(aLen);
+    for (let i = 0; i < aLen; i++) {
+      let neigh = graph.adjList[currentNode][i];
+      // console.log(neigh)
+      if (edges.has(currentNode)) {
+        let array = edges.get(currentNode);
+        let index = array.indexOf(neigh);
+        // console.log(index)
+        if (index === -1) {
+          array.push(neigh);
+          //path.push(neigh);
+          dfs(graph, neigh, endNode, edges, paths, path);
+          array = edges.get(currentNode);
+          index = array.indexOf(neigh);
+          if (index !== -1) {
+            array.splice(index, 1);
+          }
+          let len = array.length;
+          if (len === 0) {
+            edges.delete(currentNode);
+          }
+          let popped = path.pop();
+        } else {
+          let reps = countOccurrences(array, neigh);
+          if (reps === 1) {
+            array.push(neigh);
+            //path.push(neigh);
+            dfs(graph, neigh, endNode, edges, paths, path);
+            array = edges.get(currentNode);
+            index = array.indexOf(neigh);
+            if (index !== -1) {
+              array.splice(index, 1);
+            }
+            let len = array.length;
+            if (len === 0) {
+              edges.delete(currentNode);
+            }
+            let popped = path.pop();
+          }
+        }
+      } else {
+        // does not have the edge
+        edges.set(currentNode, [neigh]);
+        // path.push(neigh);
+        dfs(graph, neigh, endNode, edges, paths, path);
+        let array = edges.get(currentNode);
+        let index = array.indexOf(neigh);
+        if (index != -1) {
+          array.splice(index, 1);
+        }
+        let len = array.length;
+        if (len === 0) {
+          edges.delete(currentNode);
+        }
+        let popped = path.pop();
+      }
+    }
+  }
+}
+function countOccurrences(array, element) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+      count++;
+    }
+  }
+  return count;
+}
+function generateFinalPaths() {
+  let counter1 = 0;
+  finalPaths.forEach((value1, key) => {
+    let a1 = [...value1];
+    let flag = 0;
+    finalPaths.forEach((value2, key) => {
+      //console.log(key + ' => ' + value);
+      let a2 = [...value2];
+      //let flag1=checkArrays(a1,a2);
+      let flag1 = 0;
+      if (arraysEqual(a1, a2) === 0) {
+        // not eaqual arrays
+        flag1 = checkArrays(a1, a2);
+        if (flag1 === 0) {
+          //fpath.push([...a1]);
+
+          flag = 1;
+        }
+      }
+
+      counter1 = counter1 + 1;
+    });
+    if (flag === 0) {
+      fpath.push([...a1]);
+    }
+  }); // creating fpaths
+  let counter2 = 0;
+  finalPaths.forEach((value1, key) => {
+    let a1 = [...value1];
+    let flag = 0;
+    finalPaths.forEach((value2, key) => {
+      //console.log(key + ' => ' + value);
+      let a2 = [...value2];
+      //let flag1=checkArrays(a1,a2);
+      let flag1 = 0;
+      if (arraysEqual(a1, a2) === 0) {
+        // not eaqual arrays
+        flag1 = checkArrays1(a1, a2);
+        if (flag1 === 0) {
+          //fpath.push([...a1]);
+          if (a1.length > a2.length) {
+            flag = 1;
+          }
+        }
+      }
+
+      counter2 = counter2 + 1;
+    });
+    if (flag === 0) {
+      fpath1.push([...a1]);
+    }
+  }); // creating fpaths
+  let counter3 = 0;
+  for (let i = 0; i < fpath1.length; i++) {
+    let a1 = [...fpath1[i]];
+    let flag = 0;
+    for (let j = 0; j < fpath1.length; j++) {
+      let a2 = [...fpath1[j]];
+      let flag1 = 0;
+      if (arraysEqual(a1, a2) === 0) {
+        // not eaqual arrays
+        flag1 = checkArrays1(a1, a2);
+        if (flag1 === 0) {
+          //fpath.push([...a1]);
+          if (a1.length <= a2.length) {
+            flag = 1;
+          }
+        }
+      }
+    }
+    if (flag === 0) {
+      fpath2.push([...a1]);
+    }
+  }
+
+  let counter4 = 0;
+  for (let i = 0; i < fpath2.length; i++) {
+    let a1 = [...fpath2[i]];
+    let flag = 0;
+    for (let j = 0; j < fpath2.length; j++) {
+      let a2 = [...fpath2[j]];
+      let flag1 = 0;
+      if (arraysEqual(a1, a2) === 0) {
+        // not eaqual arrays
+        flag1 = checkArrays2(a1, a2);
+        if (flag1 === 0) {
+          //fpath.push([...a1]);
+          if (a1.length <= a2.length) {
+            flag = 1;
+          }
+        }
+      }
+    }
+    if (flag === 0) {
+      fpath3.push([...a1]);
+    }
+  }
+}
+function arraysEqual(a, b) {
+  if (a.length !== b.length) {
+    return 0;
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+function checkArrays(a1, a2) {
+  let l1 = a1.length;
+  let l2 = a2.length;
+  let flag = 0; // to discard it
+  for (let i = 0; i <= l1 - 3; i++) {
+    let a = a1[i];
+    let b = a1[i + 1];
+    let c = a1[i + 2];
+    let flag1 = 0;
+    for (let j = 0; j <= l2 - 3; j++) {
+      let x = a2[j];
+      let y = a2[j + 1];
+      let z = a2[j + 2];
+      if (a === x && b === y && c === z) {
+        flag1 = 1;
+        break;
+      }
+    }
+    if (flag1 === 0) {
+      flag = 1;
+      break;
+    }
+  }
+  return flag;
+}
+function checkArrays1(a1, a2) {
+  let l1 = a1.length;
+  let l2 = a2.length;
+  let flag = 0; // to discard it
+  for (let i = 0; i <= l1 - 2; i++) {
+    let a = a1[i];
+    let b = a1[i + 1];
+
+    let flag1 = 0;
+    for (let j = 0; j <= l2 - 2; j++) {
+      let x = a2[j];
+      let y = a2[j + 1];
+
+      if (a === x && b === y) {
+        flag1 = 1;
+        break;
+      }
+    }
+    if (flag1 === 0) {
+      flag = 1;
+      break;
+    }
+  }
+  return flag;
+}
+function checkArrays2(a1, a2) {
+  let l1 = a1.length;
+  let l2 = a2.length;
+  let flag = 0; // to discard it
+  for (let i = 0; i <= l1 - 1; i++) {
+    let a = a1[i];
+
+    let flag1 = 0;
+    for (let j = 0; j <= l2 - 1; j++) {
+      let x = a2[j];
+
+      if (a === x) {
+        flag1 = 1;
+        break;
+      }
+    }
+    if (flag1 === 0) {
+      flag = 1;
+      break;
+    }
+  }
+  return flag;
+}
+function checkIndex(k) {
+  for (let i = 0; i < temp.length; i++) {
+    if (k === temp[i]) {
+      return 1;
+      //break;
+    }
+  }
+  return 0;
+}
+function checkIndex1(k, temp1) {
+  for (let i = 0; i < temp1.length; i++) {
+    if (k === temp1[i]) {
+      return 1;
+      //break;
+    }
+  }
+  return 0;
+}
+function edgePairs(graph) {
+  let finalEdgePairs = [];
+  for (let vertex in graph.adjList) {
+    let edgpair = [];
+    edgpair.push(vertex);
+    let c = 1;
+    let neighbors = [...graph.adjList[vertex]];
+    for (let i = 0; i < neighbors.length; i++) {
+      c = c + 1;
+      edgpair.push(neighbors[i]);
+      let n1 = [...graph.adjList[neighbors[i]]];
+      for (let j = 0; j < n1.length; j++) {
+        edgpair.push(n1[j]);
+        finalEdgePairs.push([...edgpair]);
+        edgpair.pop();
+      }
+      edgpair.pop();
+    }
+  }
+  return finalEdgePairs;
+}
+function edgesF(graph) {
+  let finalEdges = [];
+  for (let vertex in graph.adjList) {
+    let edge = [];
+    edge.push(vertex);
+    let neighbors = [...graph.adjList[vertex]];
+    for (let i = 0; i < neighbors.length; i++) {
+      edge.push(neighbors[i]);
+      finalEdges.push([...edge]);
+      edge.pop();
+    }
+    edge.pop();
+  }
+  return finalEdges;
+}
+function nodes(graph) {
+  let finalNodes = [];
+  for (let vertex in graph.adjList) {
+    finalNodes.push(vertex);
+  }
+  return finalNodes;
+}
+function pathEdgeCoverage(fpath) {
+  let temp1 = [];
+  let len = fpath.length;
+  for (let i = 0; i < len; i++) {
+    let hit = 1;
+    let array1 = [...fpath[i]];
+    let l1 = array1.length;
+    for (let j = 0; j <= l1 - 2; j++) {
+      let a = array1[j];
+      let b = array1[j + 1];
+
+      let hit1 = 0;
+      for (let k = 0; k < len; k++) {
+        let array2 = [...fpath[k]];
+        // should not check same array or array that is deleted
+        if (arraysEqual(array1, array2) === 0) {
+          // arrrays are different
+          // check for array that is deleted
+          if (checkIndex1(k, temp1) != 1) {
+            let l2 = array2.length;
+            for (let h = 0; h <= l2 - 2; h++) {
+              let x = array2[h];
+              let y = array2[h + 1];
+
+              if (a == x && b == y) {
+                hit1 = 1;
+                break;
+              }
+            }
+          }
+        }
+        if (hit1 === 1) {
+          break;
+        }
+      }
+      if (hit1 === 0) {
+        hit = 0;
+        break;
+      }
+    }
+    if (hit !== 0) {
+      temp1.push(i);
+      //console.log(fpath[i]);
+    }
+  }
+  return temp1;
+}
+function pathNodeCoverage(fpath) {
+  let temp2 = [];
+  let len = fpath.length;
+  for (let i = 0; i < len; i++) {
+    let hit = 1;
+    let array1 = [...fpath[i]];
+    let l1 = array1.length;
+    for (let j = 0; j <= l1 - 1; j++) {
+      let a = array1[j];
+
+      let hit1 = 0;
+      for (let k = 0; k < len; k++) {
+        let array2 = [...fpath[k]];
+        // should not check same array or array that is deleted
+        if (arraysEqual(array1, array2) === 0) {
+          // arrrays are different
+          // check for array that is deleted
+          if (checkIndex1(k, temp2) != 1) {
+            let l2 = array2.length;
+            for (let h = 0; h <= l2 - 1; h++) {
+              let x = array2[h];
+
+              if (a == x) {
+                hit1 = 1;
+                break;
+              }
+            }
+          }
+        }
+        if (hit1 === 1) {
+          break;
+        }
+      }
+      if (hit1 === 0) {
+        hit = 0;
+        break;
+      }
+    }
+    if (hit !== 0) {
+      temp2.push(i);
+      //console.log(fpath[i]);
+    }
+  }
+  return temp2;
+}
+function pathEdgePairCoverage() {
+  let len = fpath.length;
+  for (let i = 0; i < len; i++) {
+    let hit = 1;
+    let array1 = [...fpath[i]];
+    let l1 = array1.length;
+    for (let j = 0; j <= l1 - 3; j++) {
+      let a = array1[j];
+      let b = array1[j + 1];
+      let c = array1[j + 2];
+      let hit1 = 0;
+      for (let k = 0; k < len; k++) {
+        let array2 = [...fpath[k]];
+        // should not check same array or array that is deleted
+        if (arraysEqual(array1, array2) === 0) {
+          // arrrays are different
+          // check for array that is deleted
+          if (checkIndex(k) != 1) {
+            let l2 = array2.length;
+            for (let h = 0; h <= l2 - 3; h++) {
+              let x = array2[h];
+              let y = array2[h + 1];
+              let z = array2[h + 2];
+              if (a == x && b == y && c == z) {
+                hit1 = 1;
+                break;
+              }
+            }
+          }
+        }
+        if (hit1 === 1) {
+          break;
+        }
+      }
+      if (hit1 === 0) {
+        hit = 0;
+        break;
+      }
+    }
+    if (hit !== 0) {
+      temp.push(i);
+      console.log(fpath[i]);
+    }
+  }
+}
 export default GeneratePage;
